4f308826abd803b29e44ce250377eac44eef519b
mipimaj <157592107+mipimaj@users.noreply.github.com>
2024-04-08 22:01:41 +0200

    Add files via upload

--- discord.py
+++ discord.py
@@ -11,22 +11,56 @@ import random
 import requests
 import json
 import requests
+from io import BytesIO
+from PIL import Image
+import numpy as np
+from sklearn.cluster import KMeans
+
+def get_dominant_color(image_url, num_clusters=3):
+   # Télécharger l'image à partir du lien
+   response = requests.get(image_url)
+   image = Image.open(BytesIO(response.content))
+
+   # Le reste du script reste le même
+   image_np = np.asarray(image.resize((100, 100)), dtype=np.float32)
+   pixels = image_np.reshape(-1, 3)
+
+   # Utiliser KMeans pour trouver les couleurs les plus dominantes
+   kmeans = KMeans(n_clusters=num_clusters)
+   kmeans.fit(pixels)
+
+   # Trouver l'étiquette la plus courante (c'est-à-dire la couleur la plus dominante)
+   labels = kmeans.labels_
+   counts = np.bincount(labels)
+   dominant_color = kmeans.cluster_centers_[np.argmax(counts)]
+
+   # Convertir la couleur dominante en hexadécimal
+   dominant_color = dominant_color.astype(int)
+   hex_color = '0x{:02x}{:02x}{:02x}'.format(*dominant_color)
+
+   # Convertir la couleur hexadécimale en entier
+   int_color = int(hex_color, 16)
+
+   return int_color
 
 lien = "https://pornleaks.in/packs.php"
 
 options = Options()
 options.add_extension("AdGuard AdBlocker 4.3.35.0.crx")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(
    service=ChromeService(ChromeDriverManager().install()),
    options=options
    )
 
-webhook = "https://discord.com/api/webhooks/1226783498912792637/E35M4LRIqUBzbZVIuwEMJQ3OYi8ajNULvyDX_kKm8Sk7at91avr87a2omMGGd4V4uV4_"
+webhook = "https://discord.com/api/webhooks/1226579210097918124/1cQLRw8gQ_AM3dES0b2dnEX4hWBt5BKckz_2knkQyj4mL7JEdBbd6RsB5SKwTN9Tr026"
 nomLeaks = ""
 lienLeaks = ""
 imageLeaks = ""
-timeStop = 3600
+timeStop = 3
 
 oldName = ""
 
@@ -38,24 +72,23 @@ def timeClock():
       time.sleep(1)
       timeClock()
    else:
-      timeStop = 3600
+      timeStop = 3
       getData()
 
-def sendToDiscord():
+def sendToDiscord(colorHexa):
    global webhook, nomLeaks, lienLeaks, imageLeaks
    data = {
       "username": "Leaks KhaosRevelation",
       "embeds": [
          {
-               "title": f"{nomLeaks}",
-               "fields": [
-
-                  {"name": "lien", "value": f"{lienLeaks}", "inline": False}
-               ],
-               "color": 0x03b2f8,
                "image": {
                   "url": f"{imageLeaks}"
-            }
+               },
+               "fields": [
+                  {"name": f"{nomLeaks}", "value": f"lien du leaks ci dessous: \n{lienLeaks}", "inline": False},
+                  {"name": "Publicité", "value": f"Gagner de l'argent gratuitement et facilement : \n https://r.honeygain.me/MIGUE32364", "inline": False}
+               ],
+               "color": colorHexa,
          }
       ]
    }
@@ -119,9 +152,13 @@ def getData():
 
       lienLeaks = response_json["response"]["url"]
 
+      getColor = get_dominant_color(imageLeaks)
+
+      print(getColor)
+
       print(f"Le lien extrait est : {lienLeaks}")
             
-      sendToDiscord()
+      sendToDiscord(getColor)
    else:
       timeClock()
 
